b450b7c1743ebc2559bc60df6c5528102e33a588
Peter Galbavy <peter@ubuntu.home.wonderland.org>
2020-06-13 17:51:53 +0000

    add many works of Franced Hodgson Burnett

--- notes/convtext.go
+++ notes/convtext.go
@@ -144,9 +144,6 @@ func main() {
 
 			parts := re.Split(text, -1)
 			for p, part := range parts {
-/* 				if (p == 0) {
-					continue;
-				} */
 				prefix := fmt.Sprintf(partprefix, p) + chapprefix
 				title := fmt.Sprintf(parttitle, p) + chaptitle
 
