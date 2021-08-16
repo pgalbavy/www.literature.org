0b8dfb9750ccbcd0b85179b7481fa90717722b15
Peter Galbavy <pgalbavy@itrsgroup.com>
2021-08-16 17:12:37 +0100

    use strict changes and "var" to" let"

--- js/changelog.js
+++ js/changelog.js
@@ -2,37 +2,38 @@
  * The original repo can be found at:
  * https://github.com/pgalbavy/www.literature.org
  */
+"use strict";
 
 function loadchangelog() {
 	Changelog();
 }
 
 function lastUpdatedSort(a, b) {
-	var date1 = new Date(a.lastupdated)
-	var date2 = new Date(b.lastupdated)
+	let date1 = new Date(a.lastupdated)
+	let date2 = new Date(b.lastupdated)
 
 	return date1 == date2 ? 0 : date1 > date2 ? -1 : 1
 }
 
 function Changelog() {
 	const ATTR = "changelog";
-	for (var section of document.getElementsByTagName("section")) {
-		var file = section.getAttribute(ATTR);
+	for (let section of document.getElementsByTagName("section")) {
+		let file = section.getAttribute(ATTR);
 		if (file) {
 			/* Make an HTTP request using the attribute value as the file name: */
-			req = new XMLHttpRequest();
+			let req = new XMLHttpRequest();
 			req.onreadystatechange = function () {
 				if (this.readyState == 4) {
 					if (this.status == 200) {
 						// process JSON	
-						var changelogs = JSON.parse(this.responseText);
-						var html = "";
+						let changelogs = JSON.parse(this.responseText);
+						let html = "";
 						html += "<header class=\"w3-container w3-teal\"><h3>Recently Added Books<span class=\"w3-hide-small\">";
 						html += " (Latest First)</span></h3></header>";
 						html += "<ul class=\"w3-bar-block w3-ul w3-hoverable\">";
 
-						for (c of changelogs.sort(lastUpdatedSort).slice(0, 5)) {
-							var d = new Date(c.lastupdated);
+						for (let c of changelogs.sort(lastUpdatedSort).slice(0, 5)) {
+							let d = new Date(c.lastupdated);
 
 							html += "<li class=\"w3-bar-item w3-border w3-padding-small\">";
 							html += "<a href=\"" + c.href + "\" class=\"w3-bar-item litleft w3-button w3-large w3-padding-small\">";
